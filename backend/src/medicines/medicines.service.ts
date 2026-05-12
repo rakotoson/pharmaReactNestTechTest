acc923e7e2e47dc365fbd7879cfc1b395bc81c30
Avotra Rakotoson <navotra.rakotoson@gmail.com>
2026-05-12 20:25:26 +0300

    feat: init project and inject Medicine repository into MedicinesService

--- backend/src/medicines/medicines.service.ts
+++ backend/src/medicines/medicines.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Medicine } from './medicine.entity';
 
 @Injectable()
-export class MedicinesService {}
+export class MedicinesService {
+    constructor(
+        @InjectRepository(Medicine)
+        private readonly repo: Repository<Medicine>
+    ) {}
+}
